ac071a587736f64ebf2322eb3c15d5049ed0ff85
Nelson Cossa <ncossa754@gmail.com>
2025-11-04 01:27:42 +0200

    Criar entidades JPA com relacionamentos

---
+++ src/main/java/com/devnelson/clinica/domain/Cargo.java
@@ -0,0 +1,43 @@
+package com.devnelson.clinica.domain;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="CARGOS	")
+public class Cargo extends AbstractEntity<Long> {
+	
+	@Column(name="nome", nullable=false, unique=true, length=60)
+	private String nome; 
+	
+	@ManyToOne
+	@JoinColumn(name="id_departamento-fk")
+	private Departamento departamento;
+	
+	@OneToMany(mappedBy="cargo")
+	private List<Funcionario> funcionarios;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+	
+
+}
---
+++ src/main/java/com/devnelson/clinica/domain/Departamento.java
@@ -0,0 +1,29 @@
+package com.devnelson.clinica.domain;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="DEPARTAMENTOS	")
+public class Departamento extends AbstractEntity<Long> {
+
+	
+	@Column(name="nome", nullable=false, unique=true, length=60)
+	private String nome;
+	@OneToMany(mappedBy="departamento")
+	 private List<Cargo> cargos;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
